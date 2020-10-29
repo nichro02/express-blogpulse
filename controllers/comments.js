f3f883241724ecd4c800612a55f33a1a579c0000
nichro02 <nichro02@gmail.com>
2020-10-29 16:08:41 -0700

    activated new comment form

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,15 +5,20 @@ let router = express.Router()
 //POST /comments route to add comment to an article
 
 router.post('/', (req, res) => {
+    //console.log(req.body)
     db.comment.create({
-
+        name: req.body.name,
+        content: req.body.content,
+        articleId: parseInt(req.body.articleId)
     })
     .then((comment) =>{
-        res.redirect('/articles/:id')
+        res.redirect(`/articles/${req.body.articleId}`)
     })
     .catch((error) => {
+        console.log(error)
         res.status(400).render('main/404')
-      })
+    })
+      
 })
 
 router.get('/', (req, res) => {
